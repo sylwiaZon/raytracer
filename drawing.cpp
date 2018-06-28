5330e833e22805d8e5369888818cc8dd1c90dfa5
arkadius <sylwia.zon@gmail.com>
2018-06-29 00:50:23 +0200

    reflection and refraction

--- drawing.cpp
+++ drawing.cpp
@@ -22,7 +22,11 @@ class RaytracerDrawing:Drawing{
     Vector startDirection;
     Colour **canvas;
     Vector basisVector;
-    Colour trace(const Point &origin, const Vector &direction, const Space &space){
+    float mix(const float &a, const float &b, const float &mix) 
+	{ 
+		return b * mix + a * (1 - mix); 
+	} 
+    Colour trace(const Point &origin, const Vector &direction, const Space &space,int depth){
         float t0,t1,t=INFTY;
         int id=-1;
         for (int i=0;i<space.getSize();i++){
@@ -49,32 +53,59 @@ class RaytracerDrawing:Drawing{
         Point hit=translate(origin,d);
         Vector normalVector=space.getObject(id)->getNormalVector(hit);
         //cout << normalVector.x << " "<< normalVector.y << " "<< normalVector.z << endl;
-        int transmission;
         float bias=1e-4;
-        Colour colour;
-        for(int i=0;i<space.getSize();i++){
-            if(space.getObject(i)->emissionColour.x>0||space.getObject(i)->emissionColour.y>0||space.getObject(i)->emissionColour.z>0){
-                Vector lightVector(hit,space.getObject(i)->center);
-                lightVector.normalize();
-                transmission=1;
-
-                for(int j=0;j<space.getSize();j++){
-                    if(i!=j&&j!=id){
-                        if(space.getObject(j)->intersect(hit,lightVector,t0,t1)){
-                            transmission=0;
-                        }
-                    }
-                }
-               //out<<colour.x<<" "<<colour.y<<" "<<colour.z<<" stary\n";
-                //cout<<transmission<<" "<<lightVector.dot(normalVector)<<"\n";
-                //cout<<lightVector.dot(normalVector)<<"\n";
-                Colour c = space.getObject(id)->colour;
-                //cout<<c.x<<" "<<c.y<<" "<<c.z<<"\n";
-                colour=colour + c*space.getObject(i)->emissionColour*transmission*max(0.f,lightVector.dot(normalVector));
-                //cout<<colour.x<<" "<<colour.y<<" "<<colour.z<<"\n";
-            }
+        Object * cur = space.getObject(id);
+        bool inside = false;
+        if(direction.dot(normalVector)>0){ 
+			normalVector=normalVector*(-1);
+			inside = true;
         }
-        return colour;
+        if((cur->transparency>0||cur->reflection>0)&&depth<5){
+			float facingratio = -direction.dot(normalVector); 
+			float fresneleffect = mix(pow(1 - facingratio, 3), 1, 0.1);
+			Vector refldir = direction - normalVector * 2 * direction.dot(normalVector); 
+			refldir.normalize(); 
+			Colour reflection = trace(translate(hit,normalVector * bias), refldir, space, depth + 1); 
+			Colour refraction; 
+			if (cur->transparency) {
+				cout<<direction.x<<" "<<depth<<"\n"; 
+				float ior = 1.001, eta = (inside) ? ior : 1 / ior; // are we inside or outside the surface? 
+				float cosi = -normalVector.dot(direction); 
+				float k = 1 - eta * eta * (1 - cosi * cosi); 
+				Vector refrdir = (direction * eta) + (normalVector * (eta *  cosi - sqrt(k))); 
+				refrdir.normalize(); 
+				refraction = trace(translate(hit, normalVector * bias*(-1)), refrdir, space, depth + 1); 
+			} 
+			return ( 
+				reflection * fresneleffect * cur->reflection + 
+				refraction * (1 - fresneleffect) * cur->transparency) * cur->colour; 
+		}else{
+			int transmission;
+			Colour colour;
+			for(int i=0;i<space.getSize();i++){
+				if(space.getObject(i)->emissionColour.x>0||space.getObject(i)->emissionColour.y>0||space.getObject(i)->emissionColour.z>0){
+					Vector lightVector(hit,space.getObject(i)->center);
+					lightVector.normalize();
+					transmission=1;
+					
+					for(int j=0;j<space.getSize();j++){
+						if(i!=j&&j!=id){
+							if(space.getObject(j)->intersect(hit,lightVector,t0,t1)){
+								transmission=0;
+							}
+						}
+					}
+				   //out<<colour.x<<" "<<colour.y<<" "<<colour.z<<" stary\n";
+					//cout<<transmission<<" "<<lightVector.dot(normalVector)<<"\n";
+					//cout<<lightVector.dot(normalVector)<<"\n";
+					Colour c = space.getObject(id)->colour;
+					//cout<<c.x<<" "<<c.y<<" "<<c.z<<"\n";
+					colour=colour + c*space.getObject(i)->emissionColour*transmission*max(0.f,lightVector.dot(normalVector));
+					//cout<<colour.x<<" "<<colour.y<<" "<<colour.z<<"\n";
+				}
+			}
+			return colour+cur->emissionColour;
+		}
     }
     void render(const Space &space){
         float aspectRatio=(float)width/(float)height;
@@ -100,7 +131,7 @@ class RaytracerDrawing:Drawing{
                 ray.normalize();
                 //cout << ray.x << ray.y << ray.z <<endl;
                 //cout << i << " " << j << " " <<ray.x <<" "<<ray.y << " " <<ray.z <<endl;
-                canvas[i][j]=trace(origin,ray,space);
+                canvas[i][j]=trace(origin,ray,space,0);
             }
         }
     }
--- main.cpp
+++ main.cpp
@@ -7,19 +7,29 @@ int main(){
     Vector startDirection(-3,0,0);
     Vector basisVector(0,0,1);
     Point center(-3,0,-2);
-    Sphere sp(center,1,Colour(0.5,0,0),Colour());
-    Sphere sp1(Point(-4,2,-2),1,Colour(0.2,0.6,0),Colour());
-    Sphere sp2(Point(-100,1,0),50,Colour(0.2,0.6,0.2),Colour());
-    Sphere light(Point(100,-2,-5),2,Colour(0.5,0,0),Colour(1,1,1));
-    Plane pl(Point(-50,-1000,0),Vector(10,1,1),Colour(0,0.8,0.7),Colour());
-    Cylinder cyl(Point(-10,0,0),Vector(0,2,2),Vector(1,0,0),Colour (0,1,0),Colour());
-    Cone con(Point(-10,0,0),Vector(1,2,0),M_PI/8,4,Colour (0,1,0),Colour());
-    Cube cub(Point(-10,0,0),Vector(1,0,1),Vector(0,-1,0),Colour(0.2,0.6,0.2),Colour());
-    Space space(4);
+    Sphere sp(Point(-1500,-100,0),1000,Colour(0.5,0,0),Colour(),0,0);
+    Sphere sp1(Point(-4,2,-2),1,Colour(0.2,0.6,0),Colour(),0,0);
+    Sphere sp2(Point(-10,1,0),2,Colour(0.8,0.8,0.8),Colour(),0,1);
+    Sphere sp3(Point(-5,-2,2),1,Colour(0.6,0.2,0.2),Colour(),0,0);
+    Sphere light(Point(100,-30,0),2,Colour(1,1,1),Colour(3,3,3),0,0);
+    //Sphere light(Point(-3,-3,0),1,Colour(0.6,0.2,0.2),Colour(2,2,2),0,0);
+
+    Plane pl(Point(0,3,0),Vector(0,1,0),Colour(0.4,0.8,0.2),Colour(),0,0);
+    Plane pl2(Point(0,0,20),Vector(1,0,-1),Colour(0.7,0.3,0),Colour(),0,0);
+    Plane pl3(Point(0,0,-20),Vector(1,0,1),Colour(0.4,0.2,0.3),Colour(),0,0);
+
+    //Cylinder cyl(Point(-10,0,0),Vector(0,2,2),Vector(1,0,0),Colour (0,1,0),Colour());
+    //Cone con(Point(-5,0,0),Vector(1,2,1),M_PI/6,4,Colour (0,1,0),Colour(),0,0);
+    //Cube cub(Point(-10,0,0),Vector(1,0,1),Vector(0,-1,0),Colour(0.2,0.6,0.2),Colour(),0,0);
+    Space space(6);
     space.addObject(&pl);
+    space.addObject(&pl2);
+    space.addObject(&pl3);
     space.addObject(&light);
     //space.addObject(&sp1);
-	space.addObject(&cub);
+    space.addObject(&sp2);
+    //space.addObject(&con);
+	//space.addObject(&cub);
     RaytracerDrawing drawing(500,500,origin,startDirection,basisVector);
     drawing.draw(space);
 }
